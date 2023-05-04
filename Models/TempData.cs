a4a47a3710a1c8c63b561abd6bcb399a1145cb4e
Andreas Lillje <al225wg@student.lnu.se>
2023-05-04 11:26:56 +0200

    Add comments to TempData

--- Models/TempData.cs
+++ Models/TempData.cs
@@ -1,18 +1,29 @@
 using System.Text.Json.Serialization;
 using iot_cloud_service_api.Interfaces;
 
-namespace iot_cloud_service_api.Models;
-
-
+namespace iot_cloud_service_api.Models
+{
+    /// <summary>
+    /// TempData represents a single temperature and humidity data point with a timestamp.
+    /// </summary>
     public class TempData : ITempData
     {
-
+        /// <summary>
+        /// Gets or sets the temperature value of the data point.
+        /// </summary>
         [JsonPropertyName("temperature")]
         public double Temperature { get; set; }
 
+        /// <summary>
+        /// Gets or sets the humidity value of the data point.
+        /// </summary>
         [JsonPropertyName("humidity")]
         public double Humidity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the timestamp of the data point.
+        /// </summary>
         [JsonPropertyName("timestamp")]
         public DateTime Timestamp { get; set; }
     }
-
+}
