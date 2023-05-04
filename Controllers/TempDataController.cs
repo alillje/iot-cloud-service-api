00c36d0a0bb74b0cded3817a379aa73b107ec0ce
Andreas Lillje <al225wg@student.lnu.se>
2023-05-04 11:30:59 +0200

    Add XML-comments to TempDataController

--- Controllers/TempDataController.cs
+++ Controllers/TempDataController.cs
@@ -5,6 +5,9 @@ using iot_cloud_service_api.Models;
 
 namespace iot_cloud_service_api.Controllers;
 
+/// <summary>
+/// Controller class for handling temperature and humidity data requests.
+/// </summary>
 [ApiController]
 [Route("tempdata")]
 public class TempDataController : ControllerBase
@@ -18,6 +21,10 @@ public class TempDataController : ControllerBase
 
     }
 
+    /// <summary>
+    /// Retrieves a list of TempData objects from Elasticsearch.
+    /// </summary>
+    /// <returns>An IActionResult containing a list of TempData objects.</returns>
     [HttpGet("")]
     public async Task<IActionResult> Get()
     {
@@ -25,6 +32,10 @@ public class TempDataController : ControllerBase
         return Ok(response);
     }
 
+    /// <summary>
+    /// Retrieves the latest TempData object from Elasticsearch.
+    /// </summary>
+    /// <returns>An IActionResult containing the latest TempData object.</returns>
     [HttpGet("current")]
     public async Task<IActionResult> GetCurrentTempData()
     {
@@ -32,6 +43,11 @@ public class TempDataController : ControllerBase
         return Ok(response);
     }
 
+    /// <summary>
+    /// Retrieves the daily average temperature and humidity data for a given period.
+    /// </summary>
+    /// <param name="days">The number of days to look back.</param>
+    /// <returns>An IActionResult containing the daily average temperature and humidity data.</returns>
     [HttpGet("day-average")]
     public async Task<IActionResult> GetDailyAverage(int days)
     {
@@ -39,6 +55,11 @@ public class TempDataController : ControllerBase
         return Ok(response);
     }
 
+    /// <summary>
+    /// Retrieves the hourly average temperature and humidity data for a given period.
+    /// </summary>
+    /// <param name="hours">The number of hours to look back.</param>
+    /// <returns>An IActionResult containing the hourly average temperature and humidity data.</returns>
     [HttpGet("hour-average")]
     public async Task<IActionResult> GetHourlyAverage(int hours)
     {
@@ -46,6 +67,11 @@ public class TempDataController : ControllerBase
         return Ok(response);
     }
 
+    /// <summary>
+    /// Adds a TempData object to Elasticsearch.
+    /// </summary>
+    /// <param name="tempData">The TempData object to be added.</param>
+    /// <returns>An IActionResult representing the result of the operation.</returns>
     [HttpPost("")]
     public async Task<IActionResult> Add([FromBody] TempData tempData)
     {
